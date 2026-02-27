330879280219f36849a4901dd3f698febd19afdd
gnourt0307 <truongtkls8@gmail.com>
2026-02-27 21:22:17 +0700

    change from check in with MAC address to public IP

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const { createClient } = require("@supabase/supabase-js");
@@ -12,10 +12,49 @@ const supabase = createClient(
 );
 
 app.post("/checkin", async (req, res) => {
-  const ip = req.ip;
-  console.log("User IP:", ip);
+  const companyIp = process.env.COMPANY_IP;
+  const userIp = req.ip;
+
+  if (userIp !== "::1") {
+    return res.status(400).json({ message: "Invalid IP address" });
+  }
 
   res.json({ message: "Check-in successful!" });
+});
+
+app.post("/get-info", async (req, res) => {
+  const { mac } = req.body;
+
+  if (!mac) {
+    return res.status(400).json({ message: "MAC address not found" });
+  }
+
+  const { data, error } = await supabase
+    .from("devices")
+    .select(
+      `
+      id,
+      mac_address,
+      employee:employees (
+        id,
+        employee_code,
+        full_name,
+        position,
+        status
+      )
+    `,
+    )
+    .eq("mac_address", mac)
+    .single();
+
+  if (error || !data) {
+    return res.status(404).json({ message: "Device not found" });
+  }
+
+  res.json({
+    message: "Employee found",
+    employee: data.employee,
+  });
 });
 
 app.listen(3000, () => {
